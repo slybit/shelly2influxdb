7d95b34e0d7012ec52b49d208d1fa0a1dd88aa13
slybit <slybit@slybit.org>
2025-02-24 17:07:11 +0100

    wrapped axios get call in try catch

--- shelly2influxdb.js
+++ shelly2influxdb.js
@@ -33,32 +33,41 @@ const writeToInfux = (point, retentionPolicy = 'autogen') => {
  */
 const pullShellyPMData = async () => {
     for (let shelly of config.shellypm) {
-        const response = await axios.get(shelly.url);
-        for (let m of shelly.measurements) {
-            let point = {};
-            point.measurement = m.measurement;
-            point.tags = {};
-            for (var tag in m.tags) {
-                point.tags[tag] = m.tags[tag];
-            }
-            point.fields = {};
-            for (var field in m.fields) {
-                let data = parseJSONPath(m.fields[field], response.data);
-                if (data) point.fields[field] = data;
-                if (!isNaN(point.fields[field])) point.fields[field] = Number(point.fields[field]);
-            }
-    
-            if (point.measurement && Object.keys(point.fields).length > 0) {
-                writeToInfux(point);
-            } else {
-                if (!point.measurement)
-                    logger.warn('No measurement provided. Nothing sent to influx DB.');
-                if (Object.keys(point.fields).length == 0)
-                    logger.warn('Empty fields array. Nothing sent to influx DB.');
-            }
+        let attempt = 1;
+        while (attempt <= 5) {
+            try {
+                const response = await axios.get(shelly.url);
+                for (let m of shelly.measurements) {
+                    let point = {};
+                    point.measurement = m.measurement;
+                    point.tags = {};
+                    for (var tag in m.tags) {
+                        point.tags[tag] = m.tags[tag];
+                    }
+                    point.fields = {};
+                    for (var field in m.fields) {
+                        let data = parseJSONPath(m.fields[field], response.data);
+                        if (data) point.fields[field] = data;
+                        if (!isNaN(point.fields[field])) point.fields[field] = Number(point.fields[field]);
+                    }
 
+                    if (point.measurement && Object.keys(point.fields).length > 0) {
+                        writeToInfux(point);
+                    } else {
+                        if (!point.measurement)
+                            logger.warn('No measurement provided. Nothing sent to influx DB.');
+                        if (Object.keys(point.fields).length == 0)
+                            logger.warn('Empty fields array. Nothing sent to influx DB.');
+                    }
+                    // set attempt to 5 to immediately escape the while loop
+                    attempt = 6; 
+                }
+            } catch (e) {
+                logger.error('Error getting data from shelly', { 'url' : shelly.url, 'attempt' : attempt});
+                attempt++;
+            }
         }
-        
+
     }
 }
 
@@ -86,7 +95,7 @@ influx.getDatabaseNames()
         // start up the repeater
         repeater = setInterval(function () {
             pullShellyPMData();
-        }, config.interval ?  config.interval * 1000 : 60 * 1000);
+        }, config.interval ? config.interval * 1000 : 60 * 1000);
     })
     .catch(err => {
         logger.error('Error connecting to the Influx database!');
